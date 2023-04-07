3efb72d970585ffc197248ae6672b50fd3a6905d
kunduz <kunduznbkva@gmail.com>
2023-04-07 22:21:29 +0600

    Add saving image state when screen is rotated

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -8,28 +8,47 @@ import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var diceImage:ImageView
+    private lateinit var diceImage:ImageView
+    private lateinit var rollButton: Button
+    private var num = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val rollButton: Button = findViewById(R.id.btn_roll)
-        rollButton.setOnClickListener {
-            rollDice()
+        initViews()
+        if (savedInstanceState != null) {
+            val num = savedInstanceState.getInt("diceNum")
+            rollDice(num)
         }
-        diceImage = findViewById(R.id.dice_image)
+        setClickListeners()
     }
 
-    private fun rollDice() {
-        val randomInt = Random().nextInt(6)+1
-        val drawableRes = when(randomInt){
+    private fun rollDice(num:Int) {
+        val drawableRes = when(num){
             1-> R.drawable.dice_1
             2-> R.drawable.dice_2
             3-> R.drawable.dice_3
             4-> R.drawable.dice_4
             5-> R.drawable.dice_5
-            else-> R.drawable.dice_6
+           else-> R.drawable.dice_6
         }
         diceImage.setImageResource(drawableRes)
-
     }
+
+    private fun initViews(){
+        rollButton = findViewById(R.id.btn_roll)
+        diceImage = findViewById(R.id.dice_image)
+    }
+
+    private fun setClickListeners(){
+        rollButton.setOnClickListener {
+            num = Random().nextInt(6)+1
+            rollDice(num)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt("diceNum",num)
+    }
+
 }
